8d5946fc02dff136436bd2b26ebf7b2686aa06ad
9840144 <9840144@qq.com>
2021-01-01 12:02:33 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
-sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages;packages-19.07' feeds.conf.default
+#sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages;packages-19.07' feeds.conf.default
